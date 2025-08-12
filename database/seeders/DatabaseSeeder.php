cc159018e40869eaa8428a375d38d827eb94607b
mauroaicode <mauricioaicode@gmail.com>
2025-08-11 21:44:23 -0500

    Se realizan ajustes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             OrganizationSeeder::class,
             ProcessSeeder::class,
             ProcessActionSeeder::class,
+            ProcessSubjectSeeder::class,
+            MultipleInstanceProcessSeeder::class,
         ]);
     }
 }
